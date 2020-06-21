0b0e663a27177f96f04610295d33af64cbeed2cf
michael11892 <49920569+michael11892@users.noreply.github.com>
2020-06-21 08:38:31 +0300

    Automatically Installs Up-To-Date cmake

--- root_install_script.sh
+++ root_install_script.sh
@@ -1,6 +1,13 @@
 #Installing Required Packages
-sudo yum install git cmake gcc-c++ gcc binutils \
+sudo yum install git gcc-c++ gcc binutils \
 libX11-devel libXpm-devel libXft-devel libXext-devel
+
+#Installing cmake 3* 
+wget https://cmake.org/files/v3.12/cmake-3.12.3.tar.gz
+cd cmake-3.*
+./bootstrap --prefix=/usr/local
+make -j$(nproc)
+make install
 
 #Installing Optional Packages
 sudo yum install gcc-gfortran openssl-devel pcre-devel \
@@ -11,17 +18,11 @@ libxml2-devel gsl-static
 
 #Building & Configuration
 cd /usr/local/
-
 sudo git httpa://github.com/root-mirror/root.git
-
 sudo chown -R $USER root
-
 cd root && mkdir obj && cd obj && cmake .. && make -j 4
-
 source bin/thisroot.sh
-
 cd 
-
 echo "#Include PATH For CERN ROOT" >> .bashrc
 echo "export ROOTSYS=/usr/local/root/obj" >> .bashrc
 echo "export PATH=$ROOTSYS/bin:$PATH" >> .bashrc
